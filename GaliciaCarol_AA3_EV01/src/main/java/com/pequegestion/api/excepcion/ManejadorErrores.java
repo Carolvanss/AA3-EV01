45545fccfe112e70d115764ac54aeed22eb73a01
Carol Galicia <piopiolospollitos714@gmail.com>
2026-09-19 20:23:53 -0500

    Agrega manejador de errores de validacion

---
+++ GaliciaCarol_AA3_EV01/src/main/java/com/pequegestion/api/excepcion/ManejadorErrores.java
@@ -0,0 +1,26 @@
+package com.pequegestion.api.excepcion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Intercepta los errores de validacion (@Valid) de cualquier controlador
+ * y devuelve un mensaje claro en vez del error generico de Spring.
+ */
+@RestControllerAdvice
+public class ManejadorErrores {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> manejarValidacion(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+            errores.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errores);
+    }
+}
